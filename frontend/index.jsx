57c7d4cc3f74770c705b252cbd9c1668c1b667ba
kevin park <kevin.park02@gmail.com>
2021-02-24 15:48:01 -0800

    Write an entry file to render a heading for the application

---
+++ frontend/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+    ReactDOM.render(<h1>Bracketed Tax Engine</h1>, root);
+}); 
