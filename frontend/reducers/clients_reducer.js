9a96a9c91b3d51783f8da02b6687addb06356566
kevin park <kevin.park02@gmail.com>
2021-02-25 15:36:50 -0800

    Configure the clients reducer

---
+++ frontend/reducers/clients_reducer.js
@@ -0,0 +1,22 @@
+import { RECEIVE_CLIENT, REMOVE_CLIENT } from '../actions/client_actions';
+import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+
+const clientsReducer = (state = {}, action) => {
+    Object.freeze(state);
+    let nextState = Object.assign({}, state);
+
+    switch(action.type) {
+        case RECEIVE_CLIENT:
+            nextState[action.client.id] = action.client
+            return nextState;
+        case REMOVE_CLIENT:
+            delete nextState[action.clientId];
+            return nextState;
+        case RECEIVE_CURRENT_USER:
+            return Object.assign(nextState, action.currentUser.clients)
+        default:
+            return state;
+    };
+};
+
+export default clientsReducer;
