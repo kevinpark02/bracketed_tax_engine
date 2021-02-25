b8f72af19d9c2820314f37671ec76264c80761e8
kevin park <kevin.park02@gmail.com>
2021-02-25 15:18:19 -0800

    Create ajax calls for clients

---
+++ frontend/util/client_api_util.js
@@ -0,0 +1,23 @@
+import { $CombinedState } from "redux"
+
+export const createClient = (client) => {
+    return $.ajax({
+        url: `/api/clients`,
+        method: "POST",
+        data: {client}
+    });
+};
+
+export const updateClient = (client) => {
+    return $.ajax({
+        url: `/api/clients/${client.id}`,
+        method: "PATCH",
+        data: { client }
+    });
+};
+
+export const deleteClient = (clientId) => {
+    return $.ajax({
+        url: `/api/clients/${clientId}`
+    })
+}
