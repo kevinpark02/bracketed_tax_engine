fa1dbdda7065daba78c5d2711515e94dd49e6777
kevin park <kevin.park02@gmail.com>
2021-02-25 09:53:48 -0800

    Make the signup and login form to be loaded appropriately based on the url

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import NavbarContainer from "./navbar/navbar_container";
 import LoginFormContainer from "./session_form/login_form_container";
+import SignupFormContainer from "./session_form/signup_form_container";
+
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
     <header>
       <NavbarContainer />
     </header>
-    <LoginFormContainer />
-    {/* <AuthRoute path="/login" component={LoginFormContainer} /> */}
+
+    <Route path="/login" component={LoginFormContainer} />
+    <Route path="/signup" component={SignupFormContainer} />
 </div>
 );
 
