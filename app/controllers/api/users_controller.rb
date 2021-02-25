4c8537b7fd0a24f9c38f0868e4e01bc5faeb685b
kevin park <kevin.park02@gmail.com>
2021-02-24 18:30:02 -0800

    Delete the users update controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,16 +10,6 @@ class Api::UsersController < ApplicationController
         end
     end
 
-    def update
-        # debugger
-        @user = User.find_by(id: params[:id])
-        if @user.update(user_params)
-            render :show
-        else
-            render json: ["Please attach a photo"]
-        end
-    end
-
     private
     def user_params
         params.require(:user).permit(:email, :password, :photo)
