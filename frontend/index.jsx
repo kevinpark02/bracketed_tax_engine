3ccc0cdae1b19252979ce30cf8c7f330da34b3ba
kevin park <kevin.park02@gmail.com>
2021-02-25 08:25:55 -0800

    Install npm packages to integrate with redux

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
+    const store = configureStore();
+
+    // For testing purposes - start
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    // For testing purposes - end
+
     ReactDOM.render(<h1>Bracketed Tax Engine</h1>, root);
 }); 
