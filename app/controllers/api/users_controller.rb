8153c4c8924fb9fb7978b0d2ec85f6751dfd5ec5
kevin park <kevin.park02@gmail.com>
2021-02-24 18:28:51 -0800

    Set up the users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,27 @@
 class Api::UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 401
+        end
+    end
+
+    def update
+        # debugger
+        @user = User.find_by(id: params[:id])
+        if @user.update(user_params)
+            render :show
+        else
+            render json: ["Please attach a photo"]
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password, :photo)
+    end
 end
