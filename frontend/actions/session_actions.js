0bba0839bc004690792941b3e897f8a6b603e972
kevin park <kevin.park02@gmail.com>
2021-02-24 19:39:15 -0800

    Create thunk session action creators

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -23,3 +23,23 @@ export const removeSessionErrors = () => ({
     type: REMOVE_SESSION_ERRORS,
 });
 
+export const login = (user) => (dispatch) => {
+  return SessionApiUtil.login(user).then(
+    (user) => dispatch(receiveCurrentUser(user)),
+    (errors) => dispatch(receiveSessionErrors(errors.responseJSON))
+  );
+};
+
+export const logout = () => (dispatch) => {
+  return SessionApiUtil.logout().then(
+    () => dispatch(logoutCurrentUser()),
+    (errors) => dispatch(receiveSessionErrors(errors.responseJSON))
+  );
+};
+
+export const signup = (user) => (dispatch) => {
+  return SessionApiUtil.signup(user).then(
+    (user) => dispatch(receiveCurrentUser(user)),
+    (errors) => dispatch(receiveSessionErrors(errors.responseJSON))
+  );
+};
