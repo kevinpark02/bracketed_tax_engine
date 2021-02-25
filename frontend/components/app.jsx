608d12e37cd509b88c0651851b685f639b5ac532
kevin park <kevin.park02@gmail.com>
2021-02-25 15:57:59 -0800

    Update app.jsx to show client index container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,7 @@ import NavbarContainer from "./navbar/navbar_container";
 import LoginFormContainer from "./session_form/login_form_container";
 import SignupFormContainer from "./session_form/signup_form_container";
 import Dashboard from "./dashboard/dashboard";
+import ClientIndexContainer from "./client/client_index_container";
 
 import { Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
@@ -17,6 +18,7 @@ const App = () => (
     <AuthRoute path="/signup" component={SignupFormContainer} />
 
     <ProtectedRoute path="/dashboard" component={Dashboard} />
+    <ProtectedRoute path="/new_client" component={ClientIndexContainer} />
 </div>
 );
 
