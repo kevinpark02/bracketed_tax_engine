2a48d1fb61da1c28241f4bb8a5ad47917f6581c2
kevin park <kevin.park02@gmail.com>
2021-02-26 06:58:07 -0800

    Validation methods and association to the user model for the lower bracket model

--- app/models/lower_bracket.rb
+++ app/models/lower_bracket.rb
@@ -10,4 +10,10 @@
 #  updated_at     :datetime         not null
 #
 class LowerBracket < ApplicationRecord
+    validates :lower_boundary, :lower_rate, :accountant_id, presence: true
+
+    belongs_to :accountant,
+        primary_key: :id,
+        foreign_key: :accountant_id,
+        class_name: :User
 end
