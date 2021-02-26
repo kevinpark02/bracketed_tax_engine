1a8363f5c68e15f0dfc0339830b196589756c673
kevin park <kevin.park02@gmail.com>
2021-02-26 07:23:27 -0800

    Update the entities reducer with lower bracket reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import usersReducer from "./users_reducer";
 import clientsReducer from "./clients_reducer";
 import bracketsReducer from "./brackets_reducer";
+import lowerBracketReducer from "./lower_brackets_reducer";
  
 const entitiesReducer = combineReducers({
   users: usersReducer,
   clients: clientsReducer,
   brackets: bracketsReducer,
+  lowerBracket: lowerBracketReducer
 });
 
 export default entitiesReducer;
