0178388eceba20817c2b9ff6b5eedcc3b3a98988
kevin park <kevin.park02@gmail.com>
2021-02-25 21:43:10 -0800

    Configure the brackets controller

--- app/controllers/api/brackets_controller.rb
+++ app/controllers/api/brackets_controller.rb
@@ -1,2 +1,39 @@
 class Api::BracketsController < ApplicationController
+    before_action :require_logged_in, only: [:create, :update, :destroy]
+
+    def create
+        @bracket = Bracket.new(bracket_params)
+
+        if @bracket.save
+            render :show
+        else
+            render json: @bracket.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @bracket = Bracket.find_by(id: params[:id])
+
+        if @bracket && @bracket.update(bracket_params)
+            render :show
+        else
+            render json: @bracket.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @bracket = current_user.brackets.find_by(id: params[:id])
+
+        if @bracket && @bracket.destroy
+            render json: ["You have deleted your bracket"]
+        else
+            render json: ["You can't destroy someone else's bracket"], status: 401
+        end
+    end
+
+    private
+    
+    def bracket_params
+        params.require(:bracket).permit(:type, :boundary, :rate, :accountant_id)
+    end
 end
