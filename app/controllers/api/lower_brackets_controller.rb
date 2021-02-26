9a28b9e43100ec59c86ec8486b8fd438790ff191
kevin park <kevin.park02@gmail.com>
2021-02-26 07:05:48 -0800

    Configure lower boundary bracket controller

---
+++ app/controllers/api/lower_brackets_controller.rb
@@ -0,0 +1,39 @@
+class Api::LowerBracketsController < ApplicationController
+    before_action :require_logged_in, only: [:create, :update, :destroy]
+
+    def create
+        @lower_bracket = LowerBracket.new(lower_bracket_params)
+
+        if @lower_bracket.save
+            render :show
+        else
+            render json: @lower_bracket.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @lower_bracket = LowerBracket.find_by(id: params[:id])
+
+        if @lower_bracket && @lower_bracket.update(lower_bracket_params)
+            render :show
+        else
+            render json: @lower_bracket.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @lower_bracket = current_user.lower_bracket.find_by(id: params[:id])
+
+        if @lower_bracket && @lower_bracket.destroy
+            render json: ["You have deleted your lower boundary bracket"]
+        else
+            render json: ["You can't destroy someone else's lower boundary bracket"], status: 401
+        end
+    end
+
+    private
+    
+    def lower_bracket_params
+        params.require(:lower_bracket).permit(:lower_boundary, :lower_rate, :accountant_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'lower_brackets/controller'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
 
@@ -6,6 +9,7 @@ Rails.application.routes.draw do
     resources :users, only: [:show, :create, :update, :show]
     resources :clients, only: [:create, :update, :destroy]
     resources :brackets, only: [:create, :update, :destroy]
+    resources :lower_brackets, only: [:create, :update, :destroy]
     resource :session, only: [:create, :destroy]
   end
 end
