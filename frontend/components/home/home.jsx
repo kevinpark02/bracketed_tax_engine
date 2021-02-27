d21f512e9ac78cb4a7c6f1d9ef93951119f258b8
kevin park <kevin.park02@gmail.com>
2021-02-26 19:10:55 -0800

    Fix spelling mistake

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
     return(
         <div className="homepage">
             <div className="home-description">
-                <h1>[Bracketd] Tax Engine</h1>
+                <h1>[ Bracketed ] Tax Engine</h1>
                 <h2>To simplify </h2>
                 <h2>the taxing</h2>
                 <h2>process</h2>
