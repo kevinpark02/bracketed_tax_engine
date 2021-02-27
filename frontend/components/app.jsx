52b3016babbc723e3f6a86432e1a0aed8c4c1328
kevin park <kevin.park02@gmail.com>
2021-02-26 18:27:52 -0800

    design the home page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -7,8 +7,10 @@ import ClientIndexContainer from "./client/client_index_container";
 import CreateClientContainer from "./client/create_client_container";
 import BracketIndexContainer from "./bracket/bracket_index_container";
 import LowerBracketIndexContainer from "./lower_bracket/lower_bracket_index_container";
+import HomePage from "./home/home";
 
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div className="background">
@@ -16,8 +18,10 @@ const App = () => (
       <NavbarContainer />
     </header>
 
-    <AuthRoute path="/login" component={LoginFormContainer} />
-    <AuthRoute path="/signup" component={SignupFormContainer} />
+    <AuthRoute exact path="/" component={HomePage} />
+
+    <AuthRoute exact path="/login" component={LoginFormContainer} />
+    <AuthRoute exact path="/signup" component={SignupFormContainer} />
 
     <ProtectedRoute path="/dashboard" component={Dashboard} />
     <ProtectedRoute path="/new_client" component={CreateClientContainer} />
---
+++ frontend/components/home/home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const HomePage = () => {
+    return(
+        <div className="homepage">
+            <div className="home-description">
+                <h1>[Bracketd] Tax Engine</h1>
+                <h2>To simplify </h2>
+                <h2>the taxing</h2>
+                <h2>process</h2>
+                <p>Sign up now to efficiently manage your clients' tax information and ever-so-changing tax rules</p>
+            </div>
+            <div className="home-image">
+                <img src={window.homepage_splash} alt="homepage_splash"/>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
